15001be4bc7e99ee8c155cb6f3a92b83e1b8853c
Cahit Yigit <cahityit@gmail.com>
2019-10-03 00:19:39 +0300

    added lots of todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class TodoApp extends React.Component {
+  render () {
+    return (
+    <div className="container">
+      <input type="text"/>
+      <button>Add Todo</button>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+      </ul>
+      <h2>Completed Todos</h2>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <button>Remove</button>
+          <span>Hey There!</span>
+        </li>
+      </ul>
     </div>
   );
+  }
 }
 
-export default App;
+export default TodoApp;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
